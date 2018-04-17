c9f175050ce08ef059790d5b840d5b660c957474
ljj <yeshrb@126.com>
2018-04-17 23:06:29 +0800

    完成

--- main/main.js
+++ main/main.js
@@ -1,4 +1,35 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+    return {
+        taxi(){return new Taxi()}
+    }
+
 };
+class Taxi {
+   constructor() {
+       this._distance = 0;
+       this._waitingTime = 0;
+   }
+   get distance(){
+       return this._distance;
+   }
+   set distance(dis) {
+       this._distance = dis;
+   }
+   get waitingTime() {
+       return this._waitingTime;
+   }
+   set waitingTime(min){
+       this._waitingTime = min;
+   }
+   get fee(){
+       let fee = 6;
+       if(this._distance >2 && this.distance<=8)
+           fee += (this._distance-2)*0.8;
+       if(this._distance > 8) {
+           fee+= ((this._distance -2)*0.8 +(this._distance - 8)*0.4);
+       }
+       fee += 0.25*this._waitingTime;
+       return Math.round(fee);
+   }
+
+}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,55 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    let taxi;
+    beforeEach(()=>{
+        taxi = main().taxi();
+      });
+
+    it('should be 6 when taxi runs 2KM',() =>{
+        taxi.distance=2;
+        expect(taxi.fee).toBe(6);
+    });
+
+    it('should be 7 when taxi runs 3KM',() =>{
+        taxi.distance=3;
+        expect(taxi.fee).toBe(7);
+    });
+    it('should be 11 when taxi runs 8KM',() =>{
+        taxi.distance=8;
+        expect(taxi.fee).toBe(11);
+    });
+    it('should be 12 when taxi runs 9KM',() =>{
+        taxi.distance=9;
+        expect(taxi.fee).toBe(12);
+    });
+    it('should be 25 when taxi runs 20KM',() =>{
+        taxi.distance=20;
+        expect(taxi.fee).toBe(25);
+    });
+    it('should be 25+4*0.25 = 26 when taxi runs 20KM and waiting 4 min',() =>{
+        taxi.distance=20;
+        taxi.waitingTime = 4;
+        expect(taxi.fee).toBe(26);
+    });
+    it('should be 13 when taxi runs 9KM and waiting 3 min',() =>{
+        taxi.distance=9;
+        taxi.waitingTime = 3;
+        expect(taxi.fee).toBe(13);
+    });
+    it('should be 12 when taxi runs 8KM and waiting 3 min',() =>{
+        taxi.distance=8;
+        taxi.waitingTime = 3;
+        expect(taxi.fee).toBe(12);
+    });
+    it('should be 8 when taxi runs 3KM and waiting 3 min',() =>{
+        taxi.distance=3;
+        taxi.waitingTime = 3;
+        expect(taxi.fee).toBe(8);
+    });
+    it('should be 7 when taxi runs 2KM and waiting 3 min',() =>{
+        taxi.distance=2;
+        taxi.waitingTime = 3;
+        expect(taxi.fee).toBe(7);
+    });
 });
